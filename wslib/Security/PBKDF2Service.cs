17751fa86f71d845e078ed667c12020eb8d79541
Dennis Schröter <dennis.schroeter.1@web.de>
2021-05-30 01:03:36 +0200

    Implemented struct PasswordHash

---
+++ wslib/Security/PBKDF2Service.cs
@@ -0,0 +1,55 @@
+﻿using System.Security;
+using System.Security.Cryptography;
+
+namespace wslib.Security
+{
+    /// <summary>
+    /// Has Methods for hashing password with Rfc2898DerivedBytes.
+    /// </summary>
+    public static class PBKDF2Service
+    {
+        #region Methods
+        /// <summary>
+        /// Generates a Hash(256bit) from an given password, an random generated Salt(256bit) and random generated iterations (Between 10K and 100K).
+        /// </summary>
+        /// <param name="password">The password to Hash</param>
+        /// <returns>The Hashed password with salt and iterations as struct PasswordHash</returns>
+        public static PasswordHash HashPassword(SecureString password)
+        {
+            PasswordHash result = new();
+            RNGCryptoServiceProvider rng = new();
+            result.Salt = new byte[32];
+            rng.GetBytes(result.Salt);
+            result.Iterations = GetRandomInt();
+            Rfc2898DeriveBytes deriveBytes = new Rfc2898DeriveBytes(password.ToString(), result.Salt, result.Iterations);
+            result.Hash = deriveBytes.GetBytes(32);
+            return result;
+        }
+
+        public static PasswordHash HashPassword(SecureString password, int iterations)
+        {
+            PasswordHash result = new();
+            RNGCryptoServiceProvider rng = new();
+            result.Salt = new byte[32];
+            rng.GetBytes(result.Salt);
+            result.Iterations = iterations;
+            Rfc2898DeriveBytes deriveBytes = new Rfc2898DeriveBytes(password.ToString(), result.Salt, result.Iterations);
+            result.Hash = deriveBytes.GetBytes(32);
+            return result;
+        }
+
+        public static PasswordHash HashPassword(SecureString password, int iterations, byte[] salt)
+        {
+            PasswordHash result = new();
+            result.Salt = salt;
+            result.Iterations = iterations;
+            Rfc2898DeriveBytes deriveBytes = new Rfc2898DeriveBytes(password.ToString(), result.Salt, result.Iterations);
+            result.Hash = deriveBytes.GetBytes(32);
+            return result;
+        }
+
+        private static int GetRandomInt() => RandomNumberGenerator.GetInt32(10000, 100000);
+        #endregion
+
+    }
+}
---
+++ wslib/Security/PasswordHash.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace wslib.Security
+{
+    /// <summary>
+    /// Represents a hashed password with salt and iterations.
+    /// </summary>
+    public struct PasswordHash
+    {
+        /// <summary>
+        /// The Hashed password
+        /// </summary>
+        public byte[] Hash { get; set; }
+        /// <summary>
+        /// The iterations used for the hash
+        /// </summary>
+        public int Iterations { get; set; }
+        /// <summary>
+        /// The salt used for the hash
+        /// </summary>
+        public byte[] Salt { get; set; }
+        /// <summary>
+        /// Compares two PasswordHash objects for equality.
+        /// </summary>
+        /// <param name="left">wslib.Security.PasswordHash one</param>
+        /// <param name="right">wslib.Security.PasswordHash two</param>
+        /// <returns>true if left has the same value as right; otherwise, false.</returns>
+        public static bool operator ==(PasswordHash left, PasswordHash right)
+        {
+            return (left.Hash == right.Hash) && (left.Iterations == right.Iterations) && (left.Salt == right.Salt);
+        }
+        /// <summary>
+        /// Compares two PasswordHash objects for equality.
+        /// </summary>
+        /// <param name="left">wslib.Security.PasswordHash one</param>
+        /// <param name="right">wslib.Security.PasswordHash two</param>
+        /// <returns>true if left hasn't the same value as right; otherwise, false.</returns>
+        public static bool operator !=(PasswordHash left, PasswordHash right)
+        {
+            return (left.Hash != right.Hash) || (left.Iterations != right.Iterations) || (left.Salt != right.Salt);
+        }
+        /// <summary>
+        /// Returns a value indicating whether this instance is equal to a specified wslib.Security.PasswordHash object.
+        /// </summary>
+        /// <param name="obj">A wslib.Security.PasswordHash value to compare to this instance.</param>
+        /// <returns>true if obj has the same value as this instance; otherwise, false.</returns>
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+        /// <summary>
+        /// Returns the hash code for this instance.
+        /// </summary>
+        /// <returns>A hash code for the current wslib.Security.PasswordHash.</returns>
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Hash, Iterations, Salt);
+        }
+    }
+}
