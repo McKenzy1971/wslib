1410e32d8c2b01c86d43fb25c1f2df662d0e70ff
Dennis Schröter <dennis.schroeter.1@web.de>
2021-10-10 15:02:06 +0200

    Updated DelegateCommand. Predicate<bool> isn't needed.

---
+++ wslib/Security/Encryption/AESEncryptionService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wslib.Security.Encryption
+{
+    public class AESEncryptionService
+    {
+        public AESEncryptionService()
+        {
+
+        }
+
+        public bool Encrypt(string data, Uri destination)
+        {
+
+        }
+    }
+}
